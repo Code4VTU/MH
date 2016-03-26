30a12d901ffea0ddd3862aeb8575c2ea67bd986d
MartonHristo <xpucto00@gmail.com>
2016-03-26 12:59:19 +0200

    quick update

---
+++ func.js
@@ -0,0 +1,33 @@
+var prices = {};
+var names = {};
+var total = 0;
+
+
+function onLoad() {
+	var body_size = document.body;
+	var width = 0;
+	width = body_size.clientWidth/2 - 750;
+	document.getElementById("order").style.right  = width + "px";
+}
+
+function order() {
+
+}
+
+function LoadNewPage(pageName) {
+	if(pageName == "index") {
+	
+	}
+	else if(pageName == "menu") {
+	
+	}
+	else if(pageName == "fun") {
+	
+	}
+	else if(pageName == "news") {
+	
+	}
+	else if(pageName == "contact") {
+	
+	}
+}
